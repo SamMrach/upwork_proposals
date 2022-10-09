299a3fb8f4a7dde10e85a0e141e8e5a7b66f74ae
SamMrach <merrachsamir2000@gmail.com>
2022-10-09 13:59:33 +0100

    add proposals

--- src/app/components/utils/proposals.ts
+++ src/app/components/utils/proposals.ts
@@ -2,40 +2,110 @@ import { Proposal } from 'src/app/components/utils/Proposal';
 export const proposals: Proposal[] = [
   {
     id: 1,
-    title: 'proposal1',
-    description: ` Let's make it happen.
-    Hi i have been developing chrome extensions and web applications for the last two years,during this time,i have helped 
-    dozens of clients in over ten different industries to design and develop their own extensions in order to get better results
-    and being more effective.
-    my real strength is my ability to truly understand my client's needs and prioritize his satisfaction over everything else.
-    is that what you are looking for ?
-    best regards.`,
+    title: 'proposal sample for beginners',
+    description: ` Hi [Client name or company],
+
+    I’m Nalini and I’d like to help complete your project.
+    
+    I understand how important timely deliverables and clear communication are to working together remotely. I have been a freelancer for 3 years now and have received strong feedback on the ability to deliver high-quality work by the deadline.
+    
+    Here is a sample of my work:
+    
+    [Insert link}
+    
+    Additionally per your post, I understand the importance of the 3-day turnaround time and having the deliverables in the preferred format. I will definitely be able to meet your expectations for this particular project.
+    
+    Please let me know if you have any questions or if I can provide more samples of my work.
+    
+    Best regards,
+    
+    [YOUR_NAME].`,
     favorite: false,
     saved: false,
   },
   {
     id: 2,
-    title: 'proposal2',
+    title: 'proposal sample for web developer',
     description: `Hello,
 
-    Thank you for writing such a complete job description! I think I am a good fit for the role because I have a great deal of relevant experience both on and off Upwork. Feel free to check out my profile and I can share some other resources if it would be useful.
+    My name is [YOUR_NAME] and I’d love to help you launch your new website. I am a web developer, who has worked on over 40 different websites. I studied web development for 4 years and have worked with startups to build their web presence over the past 3 years.
     
-    Could we find some time over the next few days to discuss exactly what you’re looking for and how you best like to work with freelancers?
+    Below are a few links for websites I have built:
     
-    I look forward to speaking!`,
+    Sample link 1
+    Sample link 2
+    Sample link 3
+    Sample link 4
+    
+    If you are looking for a different type of website than the ones above, I’d be happy to tailor it to your needs. I understand you are a new company and I am happy to be flexible to make sure we work with your timeline. Could you tell me more about your target market and audience? If you are unsure, I can brainstorm with you on this point.
+    
+    Do let me know if you have any questions and I am excited to have the possibility to work on this project!
+    
+    Best regards,
+    
+    [YOUR_NAME]`,
     favorite: false,
     saved: false,
   },
   {
     id: 3,
-    title: 'proposal3',
-    description: ` Hi!
+    title: 'proposal sample for virtual assistant',
+    description: ` Hi [Client name],
 
-    I read the job description and feel that I am uniquely qualified for this job. I have a great deal of relevant experience and am confident my skill level is a match.
+    My name is Ajay and I came across your post for a virtual assistant and I’d love to apply for the position.
     
-    Could we find time over the next few days to talk about the role in more detail?
+    I have 6 years of experience as an assistant for the Executive Director of [link company], so I have a strong understanding of the importance of organization, efficient working, and timely completion of tasks. You mentioned you are looking for calendar management, reaching out to clients to set up meetings and data entry, all of which require great communication skills and follow-up. I have a track record in maintaining strong communication, attention to detail and I am not afraid of being persistent.
     
-    Thanks so much,`,
+    Here is a link to my personal website and my Instagram to learn more about me:
+    
+    [Enter in website domain]
+    [Enter in Instagram link]
+    
+    I look forward to the possible opportunity to help you reach your personal and professional goals as a virtual assistant.
+    
+    Thank you,
+    
+    [YOUR_NAME]`,
+    favorite: false,
+    saved: false,
+  },
+  {
+    id: 4,
+    title: 'proposal sample data entry',
+    description: ` Hello [Client name],
+
+    I am a detail-oriented data entry specialist and I would be interested in completing your project.
+    
+    I have been working in data entry for the past 10 years and am well versed in most data entry software. Is there a particular software this project will require? My experience has also been across sectors, from accounting to IT sales and F&B so I would easily adapt to your firm’s needs. I have attached my resume below for further details about my work.
+    
+    As you can see from my reviews on Upwork, I have been hired on this platform many times before and have received great reviews from previous clients. I work methodically and am able to deliver within the necessary deadlines.
+    
+    For this type of a project, my rate is ₹3500 for a complete handover of the deliverables. Let me know if this works for you and we can discuss timelines further.
+    
+    Sincerely,
+    
+    [YOUR_NAME]`,
+    favorite: false,
+    saved: false,
+  },
+  {
+    id: 4,
+    title: 'proposal sample for graphic designer',
+    description: `Hi [Client name]
+
+    I am a graphic designer and I’d love the opportunity to work with your brand.
+    
+    After looking at your website and social media presence, I got a clear idea of the style improvements you are looking for for your visual content. I have helped numerous brands refresh their content and look, including [example client 1 link] and [example client 2 link] who are in a similar sector.
+    
+    I have been working as a freelance graphic designer for the last 5 years and it is my full-time profession. Here is a link to my online portfolio to get a closer look at my work:
+    
+    [online portfolio link]
+    
+    My goal is to help you and your brand get to where you want to go and help discover a new audience with your content. I saw in the post that you are looking for the graphic design work to be completed within 10 business days, and that should not be a problem at all. Additionally, I would be happy to go through 5 rounds of edits after submission to make sure that you are satisfied with the work.
+    
+    I look forward to hearing from you,
+    
+    [YOUR_NAME]`,
     favorite: false,
     saved: false,
   },
